5bb526daefa95ef67ee85a883efc1022b21da881
ochosteve <ochosteve08@gmail.com>
2023-08-13 04:18:34 +0100

    cleanout project

--- src/App.js
+++ src/App.js
@@ -1,47 +1,13 @@
-import './App.css';
-import Home from './component/Home';
-import {BrowserRouter as Router,Route, Switch} from 'react-router-dom' ;
-import Navbar from './component/Navbar';
-import Create from './component/Create';
-import BLogDetails from './component/BlogDetails';
-import NotFound from './component/NotFound';
 
+const App = () => {
+ 
 
-
-
-const App=()=>
-{
-
-
-  return (  
-          <Router>    
-              <div className='App'> 
-              <Navbar/>
-              <Switch>
-                <Route exact path="/" >
-                   <Home />
-                </Route>
-                <Route path="/create" >
-                   <Create/>
-                </Route>
-                <Route path="/blogs/:id" >
-                   <BLogDetails/>
-                </Route>
-                <Route path="*" >
-                  <NotFound/>
-                </Route>
-              </Switch>
-               
-                        
-              </div>
-
-           </Router>
-       
-
-           );
-
-}
-
+  return (
+    <main>
+    
+    hello
+    </main>
+  );
+};
 
 export default App;
-
