24f6638ada7f7fc10865d788ca5f476e85225cbc
ochosteve <ochosteve08@gmail.com>
2023-08-13 05:22:12 +0100

    setup redux environment

---
+++ src/features/counter/counterSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  count: 0,
+};
+
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      console.log(state);
+      state.count += 1;
+    },
+    decrement: (state) => {
+      console.log(state);
+      state.count -= 1;
+    },
+    reset: (state) => {
+      console.log(state);
+      state.count = 0;
+    },
+  },
+});
+
+
+export const {increment, decrement, reset} = counterSlice.actions;
+
+export default counterSlice.reducer;
